ff0901dc75cd49f099e23f4f46c1073e8d59dad0
danajacobc <danajacobc@gmail.com>
2024-06-23 15:31:55 -0300

    lautiname

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import styles from "./LandingPage.module.css";
 import {Link} from 'react-router-dom';
 import pikachu from '../PokemonImages/landing/pikachu.png'
+import lautiname from '../PokemonImages/landing/lauti.png'
 
 const LandingPage = () => {
 
@@ -8,7 +9,7 @@ const LandingPage = () => {
         <div className={styles.container}>
             <div className={styles.welcome}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" width="550px"/>
-            <img src="https://fontmeme.com/temporary/70717d345958ef1705e459db24d016b7.png" width="300px"/>
+            <img src={`${lautiname}`} width="300px"/>
             {/* <div className={styles.text}><h2>Lauti</h2></div> */}
             <div className={styles.text}><h2>✨¡Bienvenido a tu Pokemundo!✨</h2></div>
             <Link to='/home'>
