5f63613993cef5056fbf1fff894c47b611eab242
danajacobc <danajacobc@gmail.com>
2024-06-23 18:35:51 -0300

    postgres

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.POSTGRES_HOST || 3000;
+const port = process.env.PORT || 3000;
 
 conn.sync({ force: false })
 .then(() => {
--- api/src/db.js
+++ api/src/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE} = process.env;
 const fs = require('fs');
 const path = require('path');
 
 const sequelize = new Sequelize(
-   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/pokemonlauti`,
+   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}`,
    {
       logging: false,
       native: false,
