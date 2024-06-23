290ff4b2d6ca21651c0d28ac3ca65b80a65dac6c
danajacobc <danajacobc@gmail.com>
2024-06-23 18:03:19 -0300

    postgres

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const port = process.env.POSTGRES_HOST || 3000;
+
 conn.sync({ force: false })
 .then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('Server levantado en puerto: 3001'); // eslint-disable-line no-console
   });
 })
